3d1c2bd01f7a7e2ff7132a314c7013d6dbb409d5
devansh dwivedi <devanshdwivedi68@gmail.com>
2024-06-13 12:53:46 +0530

    chore: business feature button added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,19 +12,25 @@ import {
   DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger
 } from '@/src/components/ui/dropdown-menu';
+import { Switch } from '@/components/ui/switch';
+import { Label } from '@/components/ui/label';
+import { Button } from './ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog';
+import { Input } from '@/src/components/ui/input';
 
 export const Navbar = () => {
   const { data: session } = useSession();
-
-  console.log('session', session);
 
   return (
     <header className="flex w-full flex-col gap-3 bg-white/95 p-3 backdrop-blur supports-[backdrop-filter]:bg-white/60 md:h-16 md:flex-row md:items-center lg:px-4">
@@ -32,37 +38,66 @@ export const Navbar = () => {
         <Link href="/" className="cursor-pointer">
           <Image src={logo} alt="neurain_logo" width={60} height={60} />
         </Link>
-        <div className="relative w-12 h-12 rounded-full bg-gray-100 overflow-hidden">
-          {session && session?.user && session?.user?.image ? (
-            <div>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <img src={session.user.image} alt="profile" />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56 mx-10">
-                  <DropdownMenuGroup>
-                    <DropdownMenuLabel>{session.user.email}</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>
-                      <Link href="/profile">
-                        <span>Profile</span>
-                      </Link>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem>
-                      <span>SignOut</span>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          ) : (
-            <Image
-              src={profile_alt}
-              alt="profile"
-              layout="fill"
-              objectFit="cover"
-            />
-          )}
+        <div className="flex items-center gap-4">
+          <div className="flex flex-col items-center gap-1">
+            {/* <Button variant="outline" className="rounded-full">
+              Business owner?
+            </Button> */}
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline" className="rounded-full">
+                  Business owner?
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>Coming soon!</DialogTitle>
+                  <DialogDescription>
+                    We are developing a feature that will enable business owners
+                    to manage every aspect of their business. Stay tuned!
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <div className="relative w-12 h-12 rounded-full bg-gray-100 overflow-hidden">
+            {session && session?.user && session?.user?.image ? (
+              <div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <img
+                      src={session.user.image}
+                      alt="profile"
+                      className="rounded-full w-full h-full object-cover"
+                    />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56 mx-10">
+                    <DropdownMenuGroup>
+                      <DropdownMenuLabel>
+                        {session.user.email}
+                      </DropdownMenuLabel>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>
+                        <Link href="/profile">
+                          <span>Profile</span>
+                        </Link>
+                      </DropdownMenuItem>
+                      <DropdownMenuItem>
+                        <span>SignOut</span>
+                      </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+            ) : (
+              <Image
+                src={profile_alt}
+                alt="profile"
+                layout="fill"
+                objectFit="cover"
+              />
+            )}
+          </div>
         </div>
       </div>
     </header>
