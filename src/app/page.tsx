fc0363cbc49bd47ab370b6d899f7d02f7e5f4c82
devansh dwivedi <devanshdwivedi68@gmail.com>
2024-06-19 10:20:08 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import {Chat} from '@/components/Chat';
-
 export default function Home() {
   return (
-    <main className='relative container flex min-h-screen flex-col'>
-      <div className=' p-4 flex h-14 items-center justify-between supports-backdrop-blur:bg-background/60 sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur'>
-        <span className='font-bold'>neurain</span>
-      </div>
-      <div className='flex flex-1 py-4'>
-        <div className='w-full'>
-          <Chat />
-        </div>
-      </div>
-    </main>
+    <main className='relative container flex min-h-screen flex-col'>s</main>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,7 @@
-import {type ClassValue, clsx} from 'clsx';
-import {twMerge} from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
-export function formattedSourceText(inputText: string) {
-  return inputText
-    .replace(/\n+/g, ' ')
-    .replace(/(\w) - (\w)/g, '$1-$2')
-    .replace(/\s+/g, ' ');
-}
